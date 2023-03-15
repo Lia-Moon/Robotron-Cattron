05bf41d6926b14081abb8b04b8a0a6fe484bdeab
Júlia Soares Vieira <juliasoaresvieira@hotmail.com>
2023-03-15 16:07:09 -0300

    melhorias

--- robotron-2000/js/main.js
+++ robotron-2000/js/main.js
@@ -2,10 +2,19 @@ const subtrair = document.querySelector("#subtrair");
 const somar = document.querySelector("#somar");
 const braco = document.querySelector("#braco");
 
-somar.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) +1;
+const controle = document.querySelectorAll(".controle-ajuste");
+const contador = document.querySelectorAll("controle-contador");
+
+controle.forEach( (elemento) =>{
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent);
+    })
 })
 
-subtrair.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) -1;
-})
+function manipulaDados(operacao) {
+    if(operacao === "-") {
+        contador.value = parseInt(contador.value) -1;
+    } else {
+        contador.value = parseInt(contador.value) +1;
+    }
+}
